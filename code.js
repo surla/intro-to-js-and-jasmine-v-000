7766ac893e5c9385e1f327f688e1361a9ba08216
Joseph Surla <josephsurla@gmail.com>
2016-02-18 09:50:20 -0800

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(flavor) {
+  return "I love " + flavor;
+}
+
+function shouting(saying) {
+  return saying.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function theTruth() {
+  return true;
+}
